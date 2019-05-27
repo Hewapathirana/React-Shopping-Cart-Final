f0ea134971b9d58c8d32d39ae9615931fb767456
dasitha <dasitha.sandaruwan@digiratina.com>
2019-05-27 15:49:47 +0530

    finished the shopping cart and tested react without redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+    state ={
+        counters: [
+            { id:1,value:0 },
+            { id:2,value:0 },
+            { id:3,value:0 },
+            { id:4,value:0 }
+
+        ]
+    }
+
+    handleDelete = (counterId) =>{
+        //to get all the counters except the one given id
+        const counters = this.state.counters.filter(c => c.id !==counterId)
+        //overide the new array in setstate
+        //since the key and the value same we can just keep counters in below setstate method this.setState({counters});
+        this.setState({counters:counters});
+    }
+
+
+    handelReset =() =>{
+        const counters = this.state.counters.map(c =>{
+            c.value = 0;
+            return c;
+        });
+
+        this.setState({counters});
+    }
+
+    handleIncrement = counter =>{
+        const counters=[...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({counters});
+    };
+
+    render()
+    {
+        return (
+            <div className="App">
+         <NavBar totalCounters={this.state.counters.filter(c=>c.value >0).length}/>
+         <main className="container" > <Counters
+                 counters={this.state.counters}
+                 onRest={this.handelReset}
+                 onIncrement={this.handleIncrement}
+                 onDelete={this.handleDelete}
+
+
+         /></main>
+            </div>
+        );
+    }
+
 }
 
 export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+
+class NavBar extends Component{
+
+    render()
+    {
+        return (
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" href="#">{this.props.totalCounters}Navbar</a>
+            </nav>
+        );
+    }
+
+}
+
+export default NavBar;
